c70564b9e0b8c24f4c7943f3ffe5e688063dde08
Rob van Bakel <robbakel@gmail.com>
2021-11-10 19:41:41 +0100

    Reformat documents

--- main/get.js
+++ main/get.js
@@ -1,7 +1,6 @@
 const axios = require("axios")
 
 const get = async (input) => {
-  
   let code = null
 
   // Filter GoTiny code from input
@@ -9,31 +8,23 @@ const get = async (input) => {
   const fullLinkRegex = /gotiny.cc\/(.{6})/
 
   if (input.length === 6) {
-
     code = input
-
   } else if (fullLinkRegex.test(input)) {
-
     const regexRes = input.match(fullLinkRegex)
     code = regexRes[1]
-
   }
 
   // Make request to API
 
   try {
-
     const res = await axios.get("https://gotiny.cc/api/" + code)
     return res.data
-
   } catch (err) {
-
     throw {
-      source: 'sdk',
-      code: 'not-found',
-      message: 'GoTiny link not found',
+      source: "sdk",
+      code: "not-found",
+      message: "GoTiny link not found",
     }
-
   }
 }
 
--- main/set.js
+++ main/set.js
@@ -1,51 +1,44 @@
-const axios = require('axios');
+const axios = require("axios")
 
 const set = async (input, opt) => {
-  if (input?.replace(/\s/g, '') === '') {
+  if (input?.replace(/\s/g, "") === "") {
     throw {
-      source: 'sdk',
-      code: 'missing-argument',
-      message: 'Empty string provided',
+      source: "sdk",
+      code: "missing-argument",
+      message: "Empty string provided",
     }
   }
 
   if (!input) {
     throw {
-      source: 'sdk',
-      code: 'missing-argument',
-      message: 'No string provided',
+      source: "sdk",
+      code: "missing-argument",
+      message: "No string provided",
     }
   }
 
   // Make request to API
-  const res = await axios.post('https://gotiny.cc/api', { input, ...opt })
+  const res = await axios.post("https://gotiny.cc/api", { input, ...opt })
   const data = res.data
 
   if (data.error) {
-
     // Send back error if found
     throw data.error
-
   } else {
-
     const output = []
 
     // Loop through API response and create array with GoTiny objects
-    data.forEach(doc => {
-
+    data.forEach((doc) => {
       output.push({
         long: doc.long,
         code: doc.code,
         tiny: `gotiny.cc/${doc.code}`,
-        link: `https://gotiny.cc/${doc.code}`
+        link: `https://gotiny.cc/${doc.code}`,
       })
-
     })
 
     return output
-
   }
-
 }
 
 module.exports = set
