721d4db21104b97e15b09c87217316d0988613fc
Rob van Bakel <robbakel@gmail.com>
2021-12-04 11:18:51 +0100

    Add extended option to get-function

--- main/get.js
+++ main/get.js
@@ -1,6 +1,6 @@
-const axios = require("axios")
+const axios = require('axios')
 
-const get = async (code) => {
+const get = async (code, opt = {}) => {
   // If full link is provided, filter code out of it
   const fullLinkRegex = /gotiny.cc\/(.{4,32})/
 
@@ -8,16 +8,22 @@ const get = async (code) => {
     code = code.match(fullLinkRegex)[1]
   }
 
-  // Make request to API
+  const requestURL = new URL('https://gotiny.cc/api/' + code)
 
+  // If extended option is present, add corresponding query params
+  if (opt.extended) {
+    requestURL.searchParams.set('format', 'json')
+  }
+
+  // Make request to API
   try {
-    const res = await axios.get("https://gotiny.cc/api/" + code)
+    const res = await axios.get(requestURL.href)
     return res.data
   } catch (err) {
     throw {
-      source: "sdk",
-      code: "not-found",
-      message: "GoTiny link not found",
+      source: 'sdk',
+      code: 'not-found',
+      message: 'GoTiny link not found',
     }
   }
 }
