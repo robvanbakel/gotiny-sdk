0389c1beca20790a7728a32fcd770cc035573f8a
Rob van Bakel <robbakel@gmail.com>
2022-03-13 14:49:26 +0100

    Add ESLint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    semi: [1, 'never'],
+    'no-throw-literal': 0,
+  },
+}
