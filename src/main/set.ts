225977ab2bd08f0f123aa0fd44dca1ea9684d153
Rob van Bakel <robbakel@gmail.com>
2022-08-28 17:18:29 +0200

    Refactor set() function argument typing

--- src/main/set.ts
+++ src/main/set.ts
@@ -16,7 +16,7 @@ export interface InputObject {
   useFallback?: boolean;
 }
 
-const set = async (input: InputObject | string) => {
+const set = async (input: InputObject | InputObject["long"]) => {
   // Throw error if no input is provided
   if (!input) {
     throw {
